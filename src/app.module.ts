73432b60ae347aa249614c6e5c92c3aeb6f084fd
alahirajeffrey <alahirajeffrey@gmail.com>
2023-06-26 12:46:16 +0100

    removed unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EmailNotificationModule } from './email-notification/email-notification.module';
 
 @Module({
   imports: [EmailNotificationModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
