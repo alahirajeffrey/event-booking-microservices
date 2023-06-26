54ad97f6e74af85a98a74dfe4ced7dffa92a3af5
alahirajeffrey <alahirajeffrey@gmail.com>
2023-06-26 12:46:28 +0100

    setup swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Notification Service')
+    .setDescription('The event booking api endpoints documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-document', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
