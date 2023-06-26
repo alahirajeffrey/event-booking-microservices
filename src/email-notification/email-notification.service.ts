9a947ba086a88e2c94be33d48af2c2139931eff4
alahirajeffrey <alahirajeffrey@gmail.com>
2023-06-26 13:02:32 +0100

    setup nodemailer

--- src/email-notification/email-notification.service.ts
+++ src/email-notification/email-notification.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { createTransport } from 'nodemailer';
+import * as Mail from 'nodemailer/lib/mailer';
 
 @Injectable()
-export class EmailNotificationService {}
+export class EmailNotificationService {
+  private nodeMailerTransport: Mail;
+  constructor(private readonly configService: ConfigService) {
+    //setup mail service provider
+    this.nodeMailerTransport = createTransport({
+      service: configService.get('EMAIL_SERVICE'),
+      auth: {
+        type: 'OAuth2',
+        user: configService.get('USER_EMAIL'),
+        clientId: configService.get('EMAIL_CLIENT_ID'),
+        clientSecret: configService.get('EMAIL_CLIENT_SECRET'),
+        refreshToken: configService.get('EMAIL_REFRESH_TOKEN'),
+      },
+    });
+  }
+  sendMail(options: Mail.Options) {
+    return this.nodeMailerTransport.sendMail(options);
+  }
+}
