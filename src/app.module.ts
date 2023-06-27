9d8824312efb424f18000c218a8454c14734b950
alahirajeffrey <alahirajeffrey@gmail.com>
2023-06-27 12:12:08 +0100

    imported config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { EmailNotificationModule } from './email-notification/email-notification.module';
 import { EmailNotificationController } from './email-notification/email-notification.controller';
 import { EmailNotificationService } from './email-notification/email-notification.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [EmailNotificationModule],
+  imports: [EmailNotificationModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [EmailNotificationController],
   providers: [EmailNotificationService],
 })
