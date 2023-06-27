cd02fa625513755163024532e7d9ee5506dce63e
alahirajeffrey <alahirajeffrey@gmail.com>
2023-06-27 10:36:30 +0100

    imported email notification controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EmailNotificationModule } from './email-notification/email-notification.module';
+import { EmailNotificationController } from './email-notification/email-notification.controller';
+import { EmailNotificationService } from './email-notification/email-notification.service';
 
 @Module({
   imports: [EmailNotificationModule],
-  controllers: [],
-  providers: [],
+  controllers: [EmailNotificationController],
+  providers: [EmailNotificationService],
 })
 export class AppModule {}
