b7b5d6586001a67ac1b7b0c129f66099955141b1
alahirajeffrey <alahirajeffrey@gmail.com>
2023-06-28 12:13:17 +0100

    code refactor

--- src/email-notification/email-notification.controller.ts
+++ src/email-notification/email-notification.controller.ts
@@ -1,32 +1,32 @@
-import { Body, Controller } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { EmailNotificationService } from './email-notification.service';
 import { SendOtpEmailDto } from './dto/send-otp.dto';
 import { SendEventDetailsEmailDto } from './dto/send-event-details.dto';
 import { SendPriceSetorUpdateEmail } from './dto/price-updated.dto';
 import { SendBookingDetailsEmailDto } from './dto/send-booking-details.dto';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('email-notification')
 export class EmailNotificationController {
   constructor(private readonly emailService: EmailNotificationService) {}
 
-  @MessagePattern({ cmd: 'send-otp' })
-  async sendOtpEail(@Body() dto: SendOtpEmailDto) {
+  @MessagePattern('send-otp')
+  async sendOtpEail(@Payload() dto: SendOtpEmailDto) {
     return this.emailService.sendOtpEmail(dto);
   }
 
-  @MessagePattern({ cmd: 'send-event-details' })
-  async sendEventDetailsEmail(@Body() dto: SendEventDetailsEmailDto) {
+  @MessagePattern('send-event-details')
+  async sendEventDetailsEmail(@Payload() dto: SendEventDetailsEmailDto) {
     return this.emailService.sendEventDetailsEmail(dto);
   }
 
-  @MessagePattern({ cmd: 'send-price-update' })
-  async sendPriceSetorUpdateEmail(@Body() dto: SendPriceSetorUpdateEmail) {
+  @MessagePattern('send-price-update')
+  async sendPriceSetorUpdateEmail(@Payload() dto: SendPriceSetorUpdateEmail) {
     return this.emailService.sendPriceSetorUpdateEmail(dto);
   }
 
-  @MessagePattern({ cmd: 'send-booking-details' })
-  async sendBookingDetailsEmail(@Body() dto: SendBookingDetailsEmailDto) {
+  @MessagePattern('send-booking-details')
+  async sendBookingDetailsEmail(@Payload() dto: SendBookingDetailsEmailDto) {
     return this.emailService.sendBookingDetailsEmail(dto);
   }
 }
